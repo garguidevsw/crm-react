c3c70a55e40465e00ec889a46296250359c94ee0
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2023-01-06 17:14:30 -0600

    Sidebar creado

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,23 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 const Layout = () => {
+    const { pathname } = useLocation()
+    console.log(pathname);
   return (
-    <div>
-        <h1 className='text-6xl font-bold'>CRM - React Router DOM</h1>
+    <div className='md:flex md:min-h-screen'>
+        <aside className='md:w-1/4 bg-blue-700 px-5 py-10'>
+            <h2 className='text-4xl font-black text-center text-white'>CRM Cliente</h2>
 
-        <Outlet />
+            <nav className='mt-10'>
+                <Link className={ `${pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to="/">Clientes</Link>
+                <Link className={`${pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to="/clientes/nuevo">Nuevo Cliente</Link>
+            </nav>
+        </aside>
+
+        <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
+            <Outlet />
+        </main>
     </div>
   )
 }
