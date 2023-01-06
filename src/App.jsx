89655ea47c5092edfcdd33845be36bc7cd05cb1e
Julio Cesar Garcia Guillen <garguidevsw@gmail.com>
2023-01-06 16:08:17 -0600

    Proyecto creado - Tailwind CSS configurado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='font-bold text-5xl'>CRM React - React Router DOM</h1>
+  )
+}
+
+export default App
